38557fe913ab9354ebde76bec2a9ef4235809349
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-01-22 16:27:14 +0530

    fix(ui): text and classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
   return (
     <div className="bg-black min-h-screen w-full relative flex items-center justify-center overflow-hidden">
@@ -9,8 +8,12 @@ export default function Home() {
       <div className="absolute left-0 right-0 bottom-[0.5cm] h-px bg-neutral-800/80" />
 
       <main className="relative z-10 p-10 text-neutral-400">
-        <h1 className="text-4xl font-bold font-display">Work in progress.</h1>
-        <p className="text-lg">im making it, its gonna come soon.</p>
+        <h1 className="text-4xl font-bold font-display text-black">
+          [rokybeast]
+        </h1>
+        <p className="flex items-center justify-center h-screen text-lg">
+          im making it, its gonna come soon.
+        </p>
       </main>
     </div>
   );
